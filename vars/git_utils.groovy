3b93b7b2d1348e2820b6c4cb24b3b0d671145588
Michel Buczynski <Michel.Buczynski@tdsecurities.com>
2018-02-20 14:22:21 -0500

    .

--- vars/git_utils.groovy
+++ vars/git_utils.groovy
@@ -24,5 +24,9 @@ def currentTags() {
     return sh(returnStdout: true, script: 'git fetch --tags &>/dev/null;git name-rev --tags --name-only HEAD').trim().replaceFirst(/\^0$/,"").replaceFirst(/^undefined$/,"")
 }
 
+def commitsCountSinceBranch(sinceBranchName) {
+    h(returnStdout: true, script: "git rev-list --no-merges --count HEAD ^origin/${sinceBranchName}").trim()
+}
+
 
 
