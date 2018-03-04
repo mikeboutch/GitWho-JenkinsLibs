e82bcce0c5d3e5bab6d2d04ffca71efb4170f0af
Michel Buczynski <Michel.Buczynski@tdsecurities.com>
2018-03-04 16:59:49 -0500

    .

--- vars/verYMX.groovy
+++ vars/verYMX.groovy
@@ -3,7 +3,7 @@ def version(){
     def currentBranchName=gitUtils.currentBranchName()
     //def currentTags=""
     //def suffixBranchName=""
-    if (currentBranchName="master") {
+    if (currentBranchName=="master") {
         currentTags=gitUtils.currentTags()
         echo "we are in master $currentTags"
         if (currentTags?.trim()){
@@ -12,10 +12,12 @@ def version(){
             //error
             return ""
         }
-    } /*else if ( suffixBranchName=currentBranchName.replaceFirst(/^(?:release|hotfix)\//,"") ){
+    } 
+    if ( currentBranchName==~/^(?:release|hotfix)\// ){
+        suffixBranchName=currentBranchName.replaceFirst(/^(?:release|hotfix)\//,""
         echo "we are in release/$suffixBranchName"
         if (suffixBranchName.?.trim()){
-            def countSince=(currentBranchName ~=/^release/?
+            countSince=(currentBranchName==~/^release/?
                 gitUtils.commitsCountSinceBranch("develop"):
                 gitUtils.commitsCountSinceBranch("master"))
             return suffixBranchName+"-rc"+(countSince>0?countsince:"")
@@ -25,5 +27,5 @@ def version(){
         }
     } else { //now
 
-    }*/
+    }
 }
