708851b2526b3a15d74e60d0d717f84f2022c69c
Michel Buczynski <Michel.Buczynski@tdsecurities.com>
2018-03-07 11:19:10 -0500

    .

--- vars/verYMX.groovy
+++ vars/verYMX.groovy
@@ -75,38 +75,30 @@ def nowYYMM() {
 
 
 def splitVersion(v) {
-    if ((m = v =~ /^(\d+)\.(\d+)(?:|\.(\d+))$/)){
-        return m[0][1..-1]<<null<<null
-    } else if ((m = v =~ /^(\d+)\.(\d+).(\d+)(?:\.(\d+)|((?:\+|-).*))$/)) {
+    if ((m = v =~ /^(\d+)\.(\d+)\.(\d+)$/)){
         return m[0][1..-1]
     } else {
-        //error "No valid version number:$v"
-        return [0, 0, 0, null, null]
+        return [0, 0, 0]
     }
 }
 
 
-def greaterVersion(v1, v2, Integer column) {
+def greaterVersion(v1, v2) {
     a1 = this.splitVersion(v1)
     a2 = this.splitVersion(v2)
-    if (a1[0] > a2[0] ||
+    println(a1)
+    println a2
+    if (a2==[0,0,0] || a1[0] > a2[0] ||
             (a1[0] == a2[0] && a1[1] > a2[1]) ||
-            (a1[0] == a2[0] && a1[1] == a2[1] && a1[2] > a2[2]) ||
-            (column == 4 && a1[0] == a2[0] && a1[1] == a2[1] && a1[2] == a2[2] && a1[3] > a2[3]) ||
-            (column == 5 && a1[0] == a2[0] && a1[1] == a2[1] && a1[2] == a2[2] && a1[3] == a2[3] && a1[3] > a2[3])) {
+            (a1[0] == a2[0] && a1[1] == a2[1] && a1[2] > a2[2])) {
         //println "a1 is greater $column"
         return v1
-    } else if (a1[0] < a2[0] ||
+    } else if (a1==[0,0,0] || a1[0] < a2[0] ||
             (a1[0] == a2[0] && a1[1] < a2[1]) ||
-            (a1[0] == a2[0] && a1[1] == a2[1] && a1[2] < a2[2]) ||
-            (column == 4 && a1[0] == a2[0] && a1[1] == a2[1] && a1[2] == a2[2] && a1[3] < a2[3]) ||
-            (column == 5 && a1[0] == a2[0] && a1[1] == a2[1] && a1[2] == a2[2] && a1[3] == a2[3] && a1[3] < a2[3])) {
+            (a1[0] == a2[0] && a1[1] == a2[1] && a1[2] < a2[2])) {
         //println "a2 is greater"
 
         return v2
     } else return v1
 }
 
-def greaterVersion(v1, v2) {
-    return this.greaterVersion(v1, v2, 3)
-}
