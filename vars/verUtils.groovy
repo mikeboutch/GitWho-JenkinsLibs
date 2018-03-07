21d69f9182d3d84afa409f49c4f1fcdfea9017f0
Michel Buczynski <Michel.Buczynski@tdsecurities.com>
2018-03-07 16:12:45 -0500

    .

--- vars/verUtils.groovy
+++ vars/verUtils.groovy
@@ -44,7 +44,6 @@ def verYMX() {
     if (currentBranchName ==~ /(?:develop|feature\/.*)/) {
         nowYYMMDD()
         def lv = this.latestVersion(gitUtils.lastedTags(), gitUtils.latestSuffixOfBranch("release"))
-        echo "debug la ${lv[0]} ${lv[1]} ${lv[2]} "
         if (lv[0].toInteger()==YY && lv[1].toInteger()==MM) {
             lv[2]+=1
             version=lv.join('.')
@@ -78,7 +77,7 @@ def nowYYMMDD() {
     YY = (new SimpleDateFormat("YY")).format(now).toInteger()
     MM = (new SimpleDateFormat("MM")).format(now).toInteger()
     DD = (new SimpleDateFormat("d")).format(now).toInteger()
-    echo "current Date: $YY $MM $DD"
+    echo "today $YY-$MM-$DD"
     return [YY, MM, DD]
 }
 
