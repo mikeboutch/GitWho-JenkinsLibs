decd006b92e62a72c9e8e62e0f5e8bfdcf41359c
Michel Buczynski <Michel.Buczynski@tdsecurities.com>
2018-08-13 15:40:36 -0400

    add sendNotifications

--- vars/sendNotifications.groovy
+++ vars/sendNotifications.groovy
@@ -10,11 +10,13 @@ def call(String buildStatus = 'STARTED') {
     // Default values
     def colorName = 'RED'
     def colorCode = '#FF0000'
-    def subject = "${buildStatus}: Job '${env.JOB_NAME} <br/> ${currentBuild.displayName} - ${env.BUILD_NUMBER} [\\${BUILD_DURATION}]"
-    def summary = "${subject} <br/> \${HIPCHAT_CHANGES_OR_CAUSE} <br/> \${BLUE_OCEAN_URL} <br/> \${TEST_REPORT_URL}"
+    def chatMessage = """${buildStatus}: Job '${env.JOB_NAME} <br/>
+         ${currentBuild.displayName} - ${env.BUILD_NUMBER} [\\${BUILD_DURATION}] <br/>
+         \${HIPCHAT_CHANGES_OR_CAUSE} <br/>
+         \${BLUE_OCEAN_URL} <br/>
+         \${TEST_REPORT_URL}"""
     def details = """<p>${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]':</p>
     <p>Check console output at &QUOT;<a href='${env.BUILD_URL}'>${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>&QUOT;</p>"""
-    echo ${WorkflowScript.BLUE_OCEAN_URL}
     // Override default values based on build status
     if (buildStatus == 'STARTED') {
         color = 'YELLOW'
@@ -30,7 +32,7 @@ def call(String buildStatus = 'STARTED') {
     // Send notifications
     //slackSend (color: colorCode, message: summary)
 
-    hipchatSend (color: color, notify: true, message: summary)
+    hipchatSend (color: color, notify: true, message: chatMessage)
 
     /*emailext (
             to: 'bitwiseman@bitwiseman.com',
