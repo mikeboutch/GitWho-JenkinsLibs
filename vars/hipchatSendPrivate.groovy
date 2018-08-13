e1e8fef6ddfbd96ae68f4363500ade7cc449ce6c
Michel Buczynski <Michel.Buczynski@tdsecurities.com>
2018-08-13 19:44:01 -0400

    add sendPrivate

---
+++ vars/hipchatSendPrivate.groovy
@@ -0,0 +1,25 @@
+#!groovy
+
+import jenkins.plugins.hipchat.HipChatNotifier;
+import com.cloudbees.plugins.credentials.*;
+import org.jenkinsci.plugins.plaincredentials.StringCredentials;
+
+def call(String messages) {
+    echo emailextrecipients([[$class: 'CulpritsRecipientProvider'], [$class: 'RequesterRecipientProvider']])
+}
+def getServer() {
+    HipChatNotifier.DescriptorImpl hipChatDesc =
+            Jenkins.getInstance().getDescriptorByType(HipChatNotifier.DescriptorImpl.class);
+    return hipChatDesc.getServer()
+}
+def getToken() {
+    HipChatNotifier.DescriptorImpl hipChatDesc =
+            Jenkins.getInstance().getDescriptorByType(HipChatNotifier.DescriptorImpl.class);
+    credentialsId = hipChatDesc.getCredentialId().toString();
+    creds = CredentialsProvider.lookupCredentials(StringCredentials.class, Jenkins.instance, null, null);
+    for (c in creds) {
+        if (c.id == credentialsId) {
+            return c.getSecret()
+        }
+    }
+}
