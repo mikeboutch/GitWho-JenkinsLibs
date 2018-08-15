0e6737b9a0e84977e510070c89b406f347371aea
Michel Buczynski <Michel.Buczynski@tdsecurities.com>
2018-08-15 12:23:14 -0400

    safeBat

--- vars/safeBat.groovy
+++ vars/safeBat.groovy
@@ -1,7 +1,11 @@
 #!groovy
 def call(String script, returnStdout=false, returnStatus=false){
-    if (!(new File("\temp\${env.GIT_COMMIT}").exists())){
-        if(!(new File( "\temp"))) bat "mkdir \\temp"
+    if (!(new File("/temp/${env.GIT_COMMIT}").exists())){
+        echo "/temp/${env.GIT_COMMIT} don't exists"
+        if(!(new File( "/temp").exists())) {
+            echo "\\temp don't exists"
+            bat "mkdir \\temp"
+        }
         bat "mklink /J \\temp\\%GIT_COMMIT% ."
     }
     script="""
