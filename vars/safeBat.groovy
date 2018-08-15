01bba42e31adec2a565a9d9a859d95c1e6397886
Michel Buczynski <Michel.Buczynski@tdsecurities.com>
2018-08-15 12:02:14 -0400

    add safeBat

--- vars/safeBat.groovy
+++ vars/safeBat.groovy
@@ -4,6 +4,10 @@ def call(String script, returnStdout=false, returnStatus=false){
         if(!(new File( "\temp"))) bat "mkdir \\temp"
         bat "mklink /J \\temp\\%GIT_COMMIT% ."
     }
+    script="""
+           cd \\temp\\%GIT_COMMIT%
+           ${script}
+           """
     return bat(script:script,returnStdout:returnStdout,returnStatus:returnStatus)
 }
 
