ed481204464d09edaa2ee4337b488a9ca25c94a8
Michel Buczynski <Michel.Buczynski@tdsecurities.com>
2019-03-14 10:32:06 -0400

    Revert "safeBat using cygpath to dos path"
    This reverts commit 971f99f119d1b2ce9a9b9f95626d49b6112c92ae.

--- vars/safeBat.groovy
+++ vars/safeBat.groovy
@@ -1,30 +1,17 @@
 #!groovy
 def call(String script, returnStdout=false, returnStatus=false){
-    // if (!(new File("/temp/${env.GIT_COMMIT}").exists())){
-    //     echo "/temp/${env.GIT_COMMIT} don't exists"
-    //     if(!(new File( "/temp").exists())) {
-    //         echo "\\temp don't exists"
-    //         bat "mkdir \\temp"
-    //     }
-    //     bat "mklink /J \\temp\\%GIT_COMMIT% ."
-    // }
-    echo script
-    def pwdDOS=sh(script:"cygpath -d -a .",returnStdout:true).trim()
-    echo "#####################$pwdDOS##############################"
+    if (!(new File("/temp/${env.GIT_COMMIT}").exists())){
+        echo "/temp/${env.GIT_COMMIT} don't exists"
+        if(!(new File( "/temp").exists())) {
+            echo "\\temp don't exists"
+            bat "mkdir \\temp"
+        }
+        bat "mklink /J \\temp\\%GIT_COMMIT% ."
+    }
     script="""
-           cd "${pwdDOS}"
+           cd \\temp\\%GIT_COMMIT%
            ${script}
            """
     return bat(script:script,returnStdout:returnStdout,returnStatus:returnStatus)
 }
-
-
-//
-// cygpath -a -d .
-
-// String path = "/root/file"
-// File file = new File(path)
-
-// println("Name: " + file.name)
-// println("Parent: " + file.parent)
 
